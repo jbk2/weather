b42ef9df7143a5ada93b7474929346581d6877d0
jbk2 <james@humanbenefits.com>
2025-02-05 17:35:48 +0100

    Correct HTML class & id usage

--- src/services/UIController.js
+++ src/services/UIController.js
@@ -4,7 +4,12 @@ export class UIController {
     console.log('today ==>>', forecast.getToday())
     console.log('day1 ==>>', forecast.getDay1())
 
-    // const [todayIcon, today
+    const [todayIcon, todayTemp, todayPrecip, todayHumid, todayWind, todayDay, todayDate, todayTime] =
+      [ document.getElementById('icon-today'), document.getElementById('temp-today'),
+        document.getElementById('precip-val'), document.getElementById('humid-val'),
+        document.getElementById('wind-val'), document.getElementById('day-today'),
+        document.getElementById('date-today'), document.getElementById('time-today'),
+      ]
   }
 
   static extractData(forecast) {
