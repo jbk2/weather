5a54b096dabb06c2b69faa5c0b6715dcfa890134
jbk2 <james@humanbenefits.com>
2025-02-07 16:12:57 +0100

    Tidy up weatherApi.js

--- src/services/weatherApi.js
+++ src/services/weatherApi.js
@@ -2,10 +2,8 @@ export class WeatherApi {
   
   static async getForecast(location) {
     try {
-      // let unixTimeSecsNow = Math.floor(Date.now() / 1000);
       let dateToday = new Date(Date.now()).toISOString().split('T')[0];
       let dateIn5Days = new Date(Date.now() + 5 * 24 * 60 * 60 * 1000).toISOString().split('T')[0];
-      // let unixTimeSecsPlus5Days = unixTimeSecsNow 
       const weatherApiKey = 'UK7QEAVD2N2BJANGP8Q3GGSFV'
       const baseUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${dateToday}/${dateIn5Days}?key=${weatherApiKey}&iconSet=icons1`;
       const response = await fetch(baseUrl);
